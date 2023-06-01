57be316e101e5735e1371c6ec9bf584b8952b2ad
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2023-06-01 23:45:49 +0530

    create using java swing got some error

--- src/GUI/CustomerForm.java
+++ src/GUI/CustomerForm.java
@@ -1,22 +1,53 @@
 package GUI;
 
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.Properties;
 
-
-public class CustomerForm {
+public class CustomerForm extends JFrame {
     private JTextField textField1;
     private JTextField textField2;
     private JButton submitButton;
     private JPanel customerForm;
-    private JComboBox comboBox1;
+    private JComboBox<String> comboBox1;
+    private JPanel calander;
 
+    private UtilDateModel model;
+    private JDatePanelImpl datePanel;
+    private static JDatePickerImpl datePicker;
+
+    public CustomerForm() {
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        model = new UtilDateModel();
+        datePanel = new JDatePanelImpl(model, p);
+        datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+        calander = new JPanel();
+        calander.add(datePicker);
+
+        customerForm = new JPanel();
+        customerForm.setLayout(new BorderLayout());
+        customerForm.add(calander, BorderLayout.NORTH);
+
+        // Add other components to customerForm panel
+
+        // Set customerForm as the content pane
+        setContentPane(customerForm);
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("CustomerForm");
-        frame.setContentPane(new CustomerForm().customerForm);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setSize(500,500);
+        SwingUtilities.invokeLater(() -> {
+            CustomerForm frame = new CustomerForm();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500, 500);
+            frame.setVisible(true);
+        });
     }
 }
